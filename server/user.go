1bf56442ad6c2335067530e95e82e92b14422a44
tieshuaihang <tieshuaihang@beyebe.com>
2022-03-30 14:02:35 +0800

    查询在线用户 修改用户名

--- server/user.go
+++ server/user.go
@@ -2,6 +2,7 @@ package server
 
 import (
 	"net"
+	"strings"
 )
 
 type User struct {
@@ -49,7 +50,41 @@ func (u *User) OffLine() {
 }
 
 func (u *User) DoMessage(msg string) {
-	u.server.BroadCast(u, msg)
+
+	if msg == "who" {
+		var onlineMsg string
+		u.server.MapLock.RLock()
+		for _, u := range u.server.OnLineMap {
+			onlineMsg += u.Name + "---"
+		}
+		onlineMsg += "在线\n"
+		u.server.MapLock.RUnlock()
+		u.SendMsg(onlineMsg)
+
+	} else if len(msg) > 7 && msg[:7] == "rename|" {
+		newName := strings.Split(msg, "|")[1]
+		_, ok := u.server.OnLineMap[newName]
+		if !ok {
+
+			u.server.MapLock.Lock()
+			delete(u.server.OnLineMap, u.Name)
+			u.server.OnLineMap[newName] = u
+			u.server.MapLock.Unlock()
+			u.Name = newName
+			u.SendMsg("修改用户名成功\n")
+		} else {
+			u.SendMsg("该用户名已经被使用\n")
+		}
+
+	} else {
+		u.server.BroadCast(u, msg)
+	}
+
+}
+
+//给当前User对应的客户端发送消息
+func (u *User) SendMsg(msg string) {
+	u.Conn.Write([]byte(msg))
 }
 
 func (u *User) ListenMessage() {
